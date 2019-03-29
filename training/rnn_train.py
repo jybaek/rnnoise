474352ee6cba04226485a897c81a698825c2c83d
jybaek <oops.jybaek@gmail.com>
2019-03-29 18:35:13 +0900

    Fixed to use tf2.0 keras

--- training/rnn_train.py
+++ training/rnn_train.py
@@ -2,31 +2,12 @@
 
 from __future__ import print_function
 
-import keras
-from keras.models import Sequential
-from keras.models import Model
-from keras.layers import Input
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import GRU
-from keras.layers import SimpleRNN
-from keras.layers import Dropout
-from keras.layers import concatenate
-from keras import losses
-from keras import regularizers
-from keras.constraints import min_max_norm
-import h5py
-
-from keras.constraints import Constraint
-from keras import backend as K
+import tensorflow as tf
+from tensorflow.keras import regularizers
+from tensorflow.keras.constraints import Constraint
+from tensorflow.keras import backend as K
 import numpy as np
-
-#import tensorflow as tf
-#from keras.backend.tensorflow_backend import set_session
-#config = tf.ConfigProto()
-#config.gpu_options.per_process_gpu_memory_fraction = 0.42
-#set_session(tf.Session(config=config))
-
+import h5py
 
 def my_crossentropy(y_true, y_pred):
     return K.mean(2*K.abs(y_true-0.5) * K.binary_crossentropy(y_pred, y_true), axis=-1)
@@ -61,6 +42,8 @@ if __name__ == '__main__':
     constraint = WeightClip(0.499)
 
     print('Build model...')
+    # ================================================================
+    """
     main_input = Input(shape=(None, 42), name='main_input')
     tmp = Dense(24, activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
     vad_gru = GRU(24, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(tmp)
@@ -74,12 +57,26 @@ if __name__ == '__main__':
     denoise_output = Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
 
     model = Model(inputs=main_input, outputs=[denoise_output, vad_output])
+    """
+    # ================================================================
+    main_input = tf.keras.layers.Input(shape=(None, 42), name='main_input')
+    input_dense = tf.keras.layers.Dense(24, activation='tanh', name='input_dense', kernel_constraint=constraint, bias_constraint=constraint)(main_input)
+    vad_gru = tf.keras.layers.GRU(24, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='vad_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(input_dense)
+    vad_output = tf.keras.layers.Dense(1, activation='sigmoid', name='vad_output', kernel_constraint=constraint, bias_constraint=constraint)(vad_gru)
+    noise_input = tf.keras.layers.concatenate([input_dense, vad_gru, main_input])
+    noise_gru = tf.keras.layers.GRU(48, activation='relu', recurrent_activation='sigmoid', return_sequences=True, name='noise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(noise_input)
+    denoise_input = tf.keras.layers.concatenate([vad_gru, noise_gru, main_input])
+    denoise_gru = tf.keras.layers.GRU(96, activation='tanh', recurrent_activation='sigmoid', return_sequences=True, name='denoise_gru', kernel_regularizer=regularizers.l2(reg), recurrent_regularizer=regularizers.l2(reg), kernel_constraint=constraint, recurrent_constraint=constraint, bias_constraint=constraint)(denoise_input)
+    denoise_output = tf.keras.layers.Dense(22, activation='sigmoid', name='denoise_output', kernel_constraint=constraint, bias_constraint=constraint)(denoise_gru)
+    model = tf.keras.models.Model(inputs=main_input, outputs=[denoise_output, vad_output])
+    # ================================================================
 
     model.compile(loss=[mycost, my_crossentropy],
                   metrics=[msse],
                   optimizer='adam', loss_weights=[10, 0.5])
 
 
+    model.summary()
     batch_size = 32
 
     print('Loading data...')
